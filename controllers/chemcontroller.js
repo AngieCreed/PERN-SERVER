caf1e69997e4b9ff51c60f2e2f09db22d4e65c90
Angie Creed <angie.creed15@gmail.com>
2018-10-17 17:06:00 -0400

    security

--- controllers/chemcontroller.js
+++ controllers/chemcontroller.js
@@ -5,7 +5,7 @@ var validateSession = require('../middleware/validate-session');
 
 
 
-router.get('/', validateSession, (req, res) => {
+router.get('/:id', validateSession, (req, res) => {
 
     Chem.findAll()
     
