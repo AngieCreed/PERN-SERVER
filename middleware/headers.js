48f1bd2e08aed37f09db6572854e68d5ca8762be
Angie Creed <angie.creed15@gmail.com>
2018-10-17 14:12:19 -0400

    changes

--- middleware/headers.js
+++ middleware/headers.js
@@ -2,6 +2,5 @@ module.exports = (req, res, next) => {
     res.header('access-control-allow-origin', '*'); //allow everything - origin of the request
     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE') //allow every method
     res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  
     next();
   }
